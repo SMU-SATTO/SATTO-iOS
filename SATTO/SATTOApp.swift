85b47feadcd520222b04141df4defd5029689975
YeongJoon <yjk7557@naver.com>
2023-12-31 00:45:27 +0900

    [Feat] 기본 설정

---
+++ SATTO/SATTOApp.swift
@@ -0,0 +1,17 @@
+//
+//  SATTOApp.swift
+//  SATTO
+//
+//  Created by 김영준 on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct SATTOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
