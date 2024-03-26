c33ea94b02bc4bb57a2908a3a818a2eab2c0c814
insung0927 <78968690+Fiddich-Dev@users.noreply.github.com>
2024-03-26 13:27:22 +0900

    [#4] asset정리
    실수로 develope브랜치에서 작업함 ㅈㅅㅈㅅ

--- SATTO/Setting/SettingView.swift
+++ SATTO/Setting/SettingView.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct SettingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+//            Raindrop()
+            MySquare()
+        }
+    }
+}
+
+
+
+struct MySquare: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.size.width, y: 0))
+        path.addLine(to: CGPoint(x: rect.size.width, y: rect.size.width))
+        path.addQuadCurve(to: CGPoint(x: rect.size.width/2, y: rect.size.width * 1.2), control: CGPoint(x: rect.size.width * 0.8, y: rect.size.width * 1.2))
+        path.addQuadCurve(to: CGPoint(x: 0, y: rect.size.width), control: CGPoint(x: rect.size.width * 0.2, y: rect.size.width * 1.2))
+        path.addLine(to: CGPoint(x: 0, y: 0))
+        path.closeSubpath()
+        
+        return path
     }
 }
 
