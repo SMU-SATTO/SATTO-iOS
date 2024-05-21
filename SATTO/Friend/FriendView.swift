9c3b267adeae9bd8a5541ead831673ada396ef20
insung0927 <78968690+Fiddich-Dev@users.noreply.github.com>
2024-05-21 09:54:32 +0900

    [#29] 친구탭 초기화면 리펙토링

--- SATTO/Friend/FriendView.swift
+++ SATTO/Friend/FriendView.swift
@@ -7,7 +7,27 @@
 
 import SwiftUI
 
+enum FriendRoute: Hashable {
+    case search
+    case followerSearch
+    case followingSearch
+    case friend
+}
 
+final class FriendNavigationPathFinder: ObservableObject {
+    static let shared = FriendNavigationPathFinder()
+    private init() { }
+    
+    @Published var path: [FriendRoute] = []
+    
+    func addPath(route: FriendRoute) {
+        path.append(route)
+    }
+    
+    func popToRoot() {
+        path = .init()
+    }
+}
 
 
 
@@ -15,199 +35,184 @@ struct FriendView: View {
     
     @State var show = false
     @Namespace var namespace
-
-    @Binding var stackPath: [Route]
+    @EnvironmentObject var navPathFinder: FriendNavigationPathFinder
+    
     var body: some View {
-//        NavigationStack(path: $stackPath) {
-            VStack(spacing: 0) {
-                HStack(spacing: 0) {
-                    if !stackPath.isEmpty {
-                        Button(action: {
-                            stackPath.popLast()
-                        }, label: {
-                            Image("Classic")
-                        })
-                        .padding(.trailing, 10)
-                    }
+        NavigationStack(path: $navPathFinder.path) {
+            ZStack {
+            
+                    Color(red: 0.92, green: 0.93, blue: 0.94)
+                        .ignoresSafeArea(.all)
                     
-                    Text("친구관리")
-                        .font(.b20)
-                        .foregroundColor(Color.gray900)
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        stackPath.append(Route.search)
-                        print(stackPath)
-                    }, label: {
-                        Image("searchIcon")
-                    })
-                }
-                .padding(.horizontal, 20)
-                .padding(.bottom, 53)
-                
-                
-                if !show {
-                    
-//                    Circle()
-//                        .frame(width: 100, height: 100)
-//                        .background(
-//                            Circle()
-//                                .fill(Color.white)
-//                                .frame(width: 105, height: 105)
-//                        )
-                    ProfileImageCell(inCircleSize: 100, outCircleSize: 105)
-                        .shadow(radius: 15, x: 0, y: 10)
-                        .padding(.bottom, -50)
-                        .zIndex(1)
-                        .matchedGeometryEffect(id: "cover", in: namespace)
-                    
-                    
-                    
-                    ZStack(alignment: .top) {
-                        Rectangle()
-                            .fill(Color.white)
-                            .frame(maxWidth: .infinity, maxHeight: .infinity)
-                            .clipShape(TopCornerRadius(radius: 40))
+                ScrollView {
+                    VStack(spacing: 0) {
                         
-                        // sb16
-                        VStack(spacing: 0) {
-                            HStack(spacing: 0) {
-                                
-                                
-                                Button(action: {
-                                    stackPath.append(Route.followerSearch)
-                                }, label: {
-                                    VStack(spacing: 0) {
-                                        Text("12")
-                                            .font(.sb16)
-                                            .foregroundColor(Color.gray800)
-                                        
-                                        // sb16
-                                        Text("팔로워")
-                                            .font(.sb16)
-                                            .foregroundColor(Color.gray500)
-                                    }
-                                })
-                                
-                                
-                                
-                                Spacer()
-                                
-                                Button(action: {
-                                    stackPath.append(Route.followingSearch)
-                                }, label: {
-                                    VStack(spacing: 0) {
-                                        Text("12")
-                                            .font(.sb16)
-                                            .foregroundColor(Color.gray800)
-                                        
-                                        // sb16
-                                        Text("팔로잉")
-                                            .font(.sb16)
-                                            .foregroundColor(Color.gray500)
-                                    }
-                                })
-                            }
-                            .padding(.top, 22)
-                            .padding(.horizontal, 51)
-                            
-                            
-                            Text("한민재 20201499")
-                                .font(.sb16)
-                                .foregroundColor(Color.gray800)
-                                .matchedGeometryEffect(id: "a", in: namespace)
-                            
-                            Text("컴퓨터과학과")
-                                .font(.m14)
-                                .foregroundColor(Color.gray600)
-                                .padding(.bottom, 23)
-                                .matchedGeometryEffect(id: "b", in: namespace)
-                            
-                            HStack(spacing: 0) {
-                                
-                                
-                                
-                                Text("졸업요건 확인")
-                                    .font(.m12)
-                                    .foregroundColor(Color.white)
-                                    .padding(.horizontal, 9)
-                                    .padding(.vertical, 7)
-                                    .background(
-                                        Rectangle()
-                                            .fill(Color.blue7)
-                                            .cornerRadius(10)
-                                    )
-                                    .padding(.trailing, 32)
-                                    .onTapGesture {
-                                        withAnimation(.spring()){
-                                            show.toggle()
-                                        }
-                                    }
-                                
-                                Text("시간표 과목수정")
-                                    .font(.m12)
-                                    .foregroundColor(Color.blue7)
-                                    .padding(.horizontal, 14)
-                                    .padding(.vertical, 7)
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(Color.blue7, lineWidth: 1)
-                                        
+                        navigationBar
+                        .padding(.bottom, 53)
+                        
+                        
+                        if !show {
+
+                            ZStack(alignment: .top) {
+                                Rectangle()
+                                    .fill(Color.white)
+                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                    .clipShape(
+                                        .rect(
+                                            topLeadingRadius: 60,
+                                            topTrailingRadius: 60
+                                        )
                                     )
                                 
+                                
+                                VStack(spacing: 0) {
+                                    HStack(spacing: 0) {
+                                        
+                                        Button(action: {
+                                            navPathFinder.addPath(route: .followerSearch)
+                                        }, label: {
+                                            VStack(spacing: 0) {
+                                                Text("12")
+                                                    .foregroundColor(Color.gray800)
+                                                
+                                                Text("팔로워")
+                                                    .foregroundColor(Color.gray500)
+                                            }
+                                            .font(.sb16)
+                                        })
+                                        
+                                        Spacer()
+                                        
+                                        ProfileImageCell(inCircleSize: 100, outCircleSize: 105)
+                                            .shadow(radius: 15, x: 0, y: 10)
+                                            .padding(.top, -72)
+                                            .zIndex(1)
+                                            .matchedGeometryEffect(id: "profile", in: namespace)
+                                        
+                                        Spacer()
+                                        
+                                        Button(action: {
+                                            navPathFinder.addPath(route: .followingSearch)
+                                        }, label: {
+                                            VStack(spacing: 0) {
+                                                Text("12")
+                                                    .foregroundColor(Color.gray800)
+                                                
+                                                Text("팔로잉")
+                                                    .foregroundColor(Color.gray500)
+                                            }
+                                            .font(.sb16)
+                                        })
+                                    }
+                                    .padding(.top, 22)
+                                    .padding(.bottom, 14)
+                                    .padding(.horizontal, 51)
+                                    
+                                    
+                                    Text("한민재 20201499")
+                                        .font(.sb16)
+                                        .foregroundColor(Color.gray800)
+                                        .matchedGeometryEffect(id: "name", in: namespace)
+                                        .padding(.bottom, 5)
+                                    
+                                    Text("컴퓨터과학과")
+                                        .font(.m14)
+                                        .foregroundColor(Color.gray600)
+                                        .padding(.bottom, 23)
+                                        .matchedGeometryEffect(id: "major", in: namespace)
+                                    
+                                    HStack(spacing: 0) {
+                                            checkGraduation
+                                            
+                                            modifyTimetable
+                                    }
+                                }
                             }
-                            
+                        }
+                        else {
+                            GraduationRequirementsView(namespace: namespace, show: $show)
                         }
                         
-                        
+                    }
+                    .navigationBarBackButtonHidden()
+                    .navigationDestination(for: FriendRoute.self) { route in
+                        switch route {
+                        case .search:
+                            SearchView(TextFieldPlacehold: "친구의 학번을 입력해 주세요")
+                        case .followerSearch:
+                            SearchView(TextFieldPlacehold: "팔로워 목록")
+                        case .followingSearch:
+                            SearchView(TextFieldPlacehold: "팔로잉 목록")
+                        case .friend:
+                            FriendView()
+                        }
                     }
                 }
-                else {
-                    GraduationRequirementsView(namespace: namespace, show: $show)
-                }
-                
             }
-            .background(Color.info20, ignoresSafeAreaEdges: .top)
-            .navigationBarBackButtonHidden()
-
+        }
     }
-}
-
-struct TopCornerRadius: Shape {
-    let radius: CGFloat
     
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        // Define the rectangular bounds
-        let width = rect.size.width
-        let height = rect.size.height
-        
-        // Define the curve for the top corners
-        path.move(to: CGPoint(x: 0, y: radius))
-        path.addQuadCurve(to: CGPoint(x: radius, y: 0),
-                          control: CGPoint(x: 0, y: 0))
-        
-        // Line to top right corner
-        path.addLine(to: CGPoint(x: width - radius, y: 0))
-        
-        // Define the curve for the top right corner
-        path.addQuadCurve(to: CGPoint(x: width, y: radius),
-                          control: CGPoint(x: width, y: 0))
-        
-        // Line to bottom right corner
-        path.addLine(to: CGPoint(x: width, y: height))
-        
-        // Line to bottom left corner
-        path.addLine(to: CGPoint(x: 0, y: height))
-        
-        // Close the path
-        path.closeSubpath()
-        
-        return path
+    var checkGraduation: some View {
+        Text("졸업요건 확인")
+            .font(.m12)
+            .foregroundColor(Color.white)
+            .padding(.horizontal, 9)
+            .padding(.vertical, 7)
+            .background(
+                Rectangle()
+                    .fill(Color(red: 0.4, green: 0.31, blue: 1.0))
+                    .cornerRadius(10)
+            )
+            .padding(.trailing, 32)
+            .onTapGesture {
+                withAnimation(.spring()){
+                    show.toggle()
+                }
+            }
+    }
+    
+    var modifyTimetable: some View {
+        Text("시간표 과목수정")
+            .font(.m12)
+            .foregroundColor(Color(red: 0.4, green: 0.31, blue: 1.0))
+            .padding(.horizontal, 14)
+            .padding(.vertical, 7)
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color(red: 0.4, green: 0.31, blue: 1.0), lineWidth: 1)
+                
+            )
+    }
+    
+    var navigationBar: some View {
+        HStack(spacing: 0) {
+            if !navPathFinder.path.isEmpty {
+                Button(action: {
+                    navPathFinder.path.popLast()
+                }, label: {
+                    Image("Classic")
+                })
+                .padding(.trailing, 10)
+            }
+            
+            Text("친구관리")
+                .font(.b20)
+                .foregroundColor(Color.gray900)
+            
+            Spacer()
+            
+            if navPathFinder.path.isEmpty {
+                Button(action: {
+                    navPathFinder.addPath(route: .search)
+                }, label: {
+                    Image("searchIcon")
+                })
+            }
+        }
+        .padding(.horizontal, 20)
     }
 }
+
 
 struct GraduationRequirementsView: View {
     
@@ -217,26 +222,21 @@ struct GraduationRequirementsView: View {
     var body: some View {
         VStack(spacing: 0) {
             HStack(spacing: 0) {
-                Circle()
-                    .frame(width: 67, height: 67)
-                    .background(
-                        Circle()
-                            .fill(Color.white)
-                            .frame(width: 70, height: 70)
-                    )
+                ProfileImageCell(inCircleSize: 67, outCircleSize: 70)
                     .shadow(radius: 15, x: 0, y: 10)
                     .padding(.leading, 10)
-                    .matchedGeometryEffect(id: "cover", in: namespace)
+                    .matchedGeometryEffect(id: "profile", in: namespace)
+                
                 VStack(alignment: .leading, spacing: 0) {
                     Text("한민재 20201499")
                         .font(.sb16)
                         .foregroundColor(Color.gray800)
-                        .matchedGeometryEffect(id: "a", in: namespace)
+                        .matchedGeometryEffect(id: "name", in: namespace)
                     
                     Text("컴퓨터과학과")
                         .font(.m14)
                         .foregroundColor(Color.gray600)
-                        .matchedGeometryEffect(id: "b", in: namespace)
+                        .matchedGeometryEffect(id: "major", in: namespace)
                 }
                 .padding(.leading, 20)
                 
@@ -350,6 +350,7 @@ struct CircularProgressView: View {
 
 
 #Preview {
-    FriendView(stackPath: .constant([.friend]))
+    FriendView()
+        .environmentObject(FriendNavigationPathFinder.shared)
 }
 
