2c22501f4b4d1ae02f8d3a9b0a788c6b81513a07
YeongJoon <yjk7557@naver.com>
2024-05-30 05:12:47 +0900

    TimetableMainView 리팩토링 및 구현

--- SATTO/Timetable/TimetableMainView.swift
+++ SATTO/Timetable/TimetableMainView.swift
@@ -10,180 +10,189 @@ import PopupView
 import DGCharts
 import JHTimeTable
 
+enum TabType {
+    case timetable
+    case credits
+}
+
 struct TimetableMainView: View {
     @Binding var stackPath: [Route]
     
-    @State private var selectedTab = "이수 학점"
+    @State private var selectedTab = TabType.timetable
     @State private var selectedPickerIndex = "총 이수학점"
     
     @State var username = "민재"
     
     @State private var isMenuOpen = false
     @State var selectedIndex = 0
-   
+    
     var body: some View {
-        ZStack {
-            VStack {
-                RoundedRectangle(cornerRadius: 0)
-                    .fill(Color(red: 0.85, green: 0.87, blue: 1))
-                    .frame(height: 200)
-                    .overlay(
-                        ZStack {
-                            VStack(alignment: .leading) {
-                                HStack(spacing: 20) {
-                                    VStack {
-                                        Button(action: {
-                                            selectedTab = "시간표"
-                                        }) {
-                                            Text("시간표")
-                                                .font(.sb14)
-                                                .foregroundStyle(.black)
-                                        }
-                                    }
-                                    VStack {
-                                        Button(action: {
-                                            selectedTab = "이수 학점"
-                                        }) {
-                                            Text("이수 학점")
-                                                .font(.sb14)
-                                                .foregroundStyle(.black)
-                                        }
-                                    }
-                                    Spacer()
-                                }
-                                
-                                VStack(alignment: .leading) {
-                                    Text("2024년 1학기 시간표가 업로드됐어요!")
-                                        .font(.b16)
-                                        .foregroundStyle(.black)
-                                        .padding(.top, 20)
-                                    
-                                    Text("민재님을 위한 시간표를 만들어 드릴게요.")
-                                        .font(.m12)
-                                        .foregroundStyle(.gray400)
-                                        .padding(.top, 5)
-                                    
-                                    Button(action: {
-                                        //MARK: - 화면 이동
-                                        stackPath.append(Route.timetableOption)
-                                    }) {
-                                        RoundedRectangle(cornerRadius: 7)
-                                            .fill(Color(red: 0.11, green: 0.33, blue: 1))
-                                            .frame(width: 120, height: 35)
-                                            .overlay(
-                                                Text("시간표 만들기 ->")
-                                                    .font(
-                                                        Font.custom("Pretendard", size: 12)
-                                                            .weight(.semibold)
-                                                    )
-                                                    .foregroundStyle(.white)
-                                            )
-                                    }
-                                    .padding(.top, 10)
-                                }
-                                
-                            }
-                            .padding(.leading, 20)
-                            HStack {
-                                Spacer()
-                                Image("TimeTableMainView_SATTO")
-                                    .resizable()
-                                    .frame(width: 180, height: 160)
-                                
-                            }
-                        }
-                    )
-                if selectedTab == "시간표" {
-                    VStack {
-                        HStack {
-                            Text("\(username)님의 이번 학기 시간표")
-                                .font(.b14)
-                                .padding(.leading, 30)
-                            Spacer()
-                            //MARK: - 오른쪽 메뉴 오픈
-                            Button(action: {
-                                isMenuOpen.toggle()
-                            }) {
-                                Image(systemName: "list.bullet")
-                            }
-                            .padding(.trailing, 10)
-                        }
-                        .padding(.top, 10)
-                        JHLectureTable {
-                            ForEach(LectureModel.examples) { lecture in
-                                ZStack(alignment: .topLeading) {
-                                    Rectangle()
-                //                        .foregroundStyle(Color(hexString: lecture.color))
-                                    Text(lecture.title)
-                                        .font(.caption)
-                                        .fontWeight(.bold)
-                                        .foregroundStyle(Color.white)
-                                        .padding(EdgeInsets(top: 2, leading: 3, bottom: 0, trailing: 0))
-                                }
-                                .lectureTableTime(week: lecture.week,
-                                                  startAt: lecture.startAt,
-                                                  endAt: lecture.endAt)
-                            }
-                        } timebar: { time in
-                            // Add customized timebar
-                            Text("\(time.hour)")
-                                .padding(.trailing, 3)
-                        } weekbar: { week in
-                            // week: LectureWeeks enum
-                            // You can use week.symbol, week.shortSymbol, week.veryShortSymbol
-                            // Add cusomized weekbar
-                            Text("\(week)")
-                                .padding(.horizontal, 0)
-                        } background: {
-                            // Add background
-                        }
-                        .lectureTableWeekdays([.sun, .mon, .tue, .wed, .thu, .fri, .sat])
-                        .lectureTableTimes(startAt: .init(hour: 8, minute: 0), endAt: .init(hour: 22, minute: 0)) // 시작, 끝 시간 설정
-                        .lectureTableBorder(width: 0.5, radius: 0, color: "#979797") // table 그리드 선 색 변경
-                        .lectureTableBar(time: .init(height: 0, width: 30), week: .init(height: 30, width: 10)) //날짜, 시간 위치 변경 가능
-                        .padding(.horizontal, 15)
-                    }
+        ScrollView {
+            ZStack {
+                VStack {
+                    headerView
+                    tabContentView
+                    Spacer()
                 }
-                else if selectedTab == "이수 학점" {
-                    HStack {
-                        Text("\(username)님의 이수 학점")
-                            .font(.b14)
-                            .padding(.leading, 30)
-                        Spacer()
-                    }
-                    .padding(.top, 10)
-                    RoundedRectangle(cornerRadius: 30)
-                        .fill(Color(red: 0.96, green: 0.96, blue: 0.98))
-                        .frame(height: 50)
-                        .overlay(
-                            HStack(spacing: 0) {
-                                ForEach(["총 이수학점", "전공", "교양필수", "교양"], id: \.self) { text in
-                                    CustomPickerView(text: text, selectedOption: $selectedPickerIndex)
-                                }
-                                .padding(.horizontal, 5)
-                            }
-                                .padding(.horizontal, 5)
-                        )
-                        .padding(.horizontal, 20)
-                    HStack {
-                        Text("졸업까지 필요한 학점")
-                            .padding(.leading, 30)
-                        Spacer()
-                    }
-                    GraduationRequirementsRadarView(entries: ChartTransaction.allTransactions.map { RadarChartDataEntry(value: $0.quantity) })
-                        .frame(width: 300, height: 300)
+            }
+            .popup(isPresented: $isMenuOpen, view: {
+                TimetableMenuView(isMenuOpen: $isMenuOpen)
+            }, customize: {
+                $0
+                    .position(.trailing)
+                    .appearFrom(.right)
+                    .backgroundColor(.black.opacity(0.5))
+            })
+        }
+    }
+    
+    private var headerView: some View {
+        UnevenRoundedRectangle(bottomLeadingRadius: 20, bottomTrailingRadius: 20)
+            .fill(Color(red: 0.85, green: 0.87, blue: 1))
+            .frame(height: 160)
+            .overlay(
+                ZStack {
+                    headerContent
+                    headerImage
                 }
+            )
+    }
+    
+    private var headerContent: some View {
+        VStack(alignment: .leading, spacing: 15) {
+            headerTabs
+            headerMessage
+            createTimetableButton
+        }
+        .padding(.leading, 20)
+    }
+    
+    private var headerTabs: some View {
+        HStack(spacing: 20) {
+            tabButton(title: "시간표", tab: .timetable)
+            tabButton(title: "이수 학점", tab: .credits)
+            Spacer()
+        }
+    }
+    
+    private var headerMessage: some View {
+        VStack(alignment: .leading, spacing: 3) {
+            Text("2024년 2학기 시간표가 업로드됐어요!")
+                .font(.b16)
+                .foregroundStyle(.black)
+            Text("\(username)님을 위한 시간표를 만들어 드릴게요.")
+                .font(.m12)
+                .foregroundStyle(.gray400)
+        }
+    }
+    
+    private var createTimetableButton: some View {
+        Button(action: {
+            stackPath.append(Route.timetableOption)
+        }) {
+            Text("시간표 만들기 ->")
+                .font(.sb12)
+                .foregroundStyle(.white)
+                .background(
+                    RoundedRectangle(cornerRadius: 7)
+                        .fill(Color("blue_7"))
+                        .padding(EdgeInsets(top: -10, leading: -15, bottom: -10, trailing: -15))
+                )
+        }
+        .padding(EdgeInsets(top: 10, leading: 15, bottom: 10, trailing: 15))
+    }
+    
+    private var headerImage: some View {
+        GeometryReader { geometry in
+            HStack {
                 Spacer()
+                Image("TimeTableMainView_SATTO")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: geometry.size.width * 0.35) // Adjust width dynamically
+                    .frame(height: geometry.size.height * 1.0) // Adjust height dynamically
+                    .padding(.trailing, 5)
             }
         }
-        .popup(isPresented: $isMenuOpen, view: {
-            TimetableMenuView(isMenuOpen: $isMenuOpen)
-        }, customize: {
-            $0
-                .position(.trailing)
-                .appearFrom(.right)
-                .backgroundColor(.black.opacity(0.5))
-        })
+    }
+    
+    private var tabContentView: some View {
+        VStack {
+            if selectedTab == .timetable {
+                timetableView
+            } else if selectedTab == .credits {
+                creditsView
+            }
+        }
+    }
+    
+    private var timetableView: some View {
+        VStack {
+            HStack {
+                Text("\(username)님의 이번 학기 시간표")
+                    .font(.b14)
+                    .padding(.leading, 30)
+                Spacer()
+                Button(action: {
+                    isMenuOpen.toggle()
+                }) {
+                    Image(systemName: "ellipsis")
+                        .foregroundStyle(.black)
+                }
+                .padding(.trailing, 20)
+            }
+            .padding(.top, 10)
+            
+            TimetableView(timetableBaseArray: [])
+            .padding(.horizontal, 15)
+        }
+    }
+    
+    private var creditsView: some View {
+        VStack {
+            HStack {
+                Text("\(username)님의 이수 학점")
+                    .font(.b14)
+                    .padding(.leading, 30)
+                Spacer()
+            }
+            .padding(.top, 10)
+            
+            RoundedRectangle(cornerRadius: 30)
+                .fill(Color(red: 0.96, green: 0.96, blue: 0.98))
+                .frame(height: 50)
+                .overlay(
+                    HStack(spacing: 0) {
+                        ForEach(["총 이수학점", "전공", "교양필수", "교양"], id: \.self) { text in
+                            CustomPickerView(text: text, selectedOption: $selectedPickerIndex)
+                        }
+                        .padding(.horizontal, 5)
+                    }
+                        .padding(.horizontal, 5)
+                )
+                .padding(.horizontal, 20)
+            
+            HStack {
+                Text("졸업까지 필요한 학점")
+                    .font(.b14)
+                    .padding(.leading, 30)
+                Spacer()
+            }
+            
+            GraduationRequirementsRadarView(entries: ChartTransaction.allTransactions.map { RadarChartDataEntry(value: $0.quantity) })
+                .frame(width: 300, height: 300)
+        }
+    }
+    
+    @ViewBuilder
+    private func tabButton(title: String, tab: TabType) -> some View {
+        Button(action: {
+            selectedTab = tab
+        }) {
+            Text(title)
+                .font(selectedTab == tab ? .sb16 : .sb14)
+                .foregroundStyle(selectedTab == tab ? Color("blue_5") : .black)
+        }
     }
 }
 
@@ -198,7 +207,7 @@ struct CustomPickerView: View {
             .overlay(
                 Text(text)
                     .font(.sb14)
-                    .foregroundStyle(selectedOption == text ? Color.blue : Color.black)
+                    .foregroundStyle(selectedOption == text ? Color("blue_4") : Color.black)
             )
             .onTapGesture {
                 withAnimation {
