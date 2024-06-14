bce9973b6d66ae1b442c671ec27fea14e91b3ba6
YeongJoon <yjk7557@naver.com>
2024-06-14 16:22:47 +0900

    TimetableMainView 리팩토링, TimetableMenuView 구현

--- SATTO/Timetable/TimetableMainView.swift
+++ SATTO/Timetable/TimetableMainView.swift
@@ -10,21 +10,23 @@ import PopupView
 import DGCharts
 import JHTimeTable
 
-enum TabType {
-    case timetable
-    case credits
-}
-
 struct TimetableMainView: View {
     @Binding var stackPath: [Route]
     
-    @State private var selectedTab = TabType.timetable
-    @State private var selectedPickerIndex = "총 이수학점"
+    @State private var selectedTab = "시간표"
+    @State private var currSelectedOption = "전공"
+    
+    @Namespace private var namespace
     
     @State var username = "민재"
     
-    @State private var isMenuOpen = false
     @State var selectedIndex = 0
+    
+    let majorCreditGoals = 72                  //전공 학점 목표
+    @State var majorCredit: Double = 60        //전공 학점
+    let GECreditGoals = 60                     //교양 학점 목표
+    @State var GECredit: Double = 33           //교양 학점
+    @State var totalCredit: Double = 130       //전체 학점
     
     var body: some View {
         ScrollView {
@@ -35,14 +37,6 @@ struct TimetableMainView: View {
                     Spacer()
                 }
             }
-            .popup(isPresented: $isMenuOpen, view: {
-                TimetableMenuView(isMenuOpen: $isMenuOpen)
-            }, customize: {
-                $0
-                    .position(.trailing)
-                    .appearFrom(.right)
-                    .backgroundColor(.black.opacity(0.5))
-            })
         }
     }
     
@@ -69,8 +63,8 @@ struct TimetableMainView: View {
     
     private var headerTabs: some View {
         HStack(spacing: 20) {
-            tabButton(title: "시간표", tab: .timetable)
-            tabButton(title: "이수 학점", tab: .credits)
+            tabButton(title: "시간표", tab: "시간표")
+            tabButton(title: "이수학점", tab: "이수학점")
             Spacer()
         }
     }
@@ -109,8 +103,8 @@ struct TimetableMainView: View {
                 Image("TimeTableMainView_SATTO")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: geometry.size.width * 0.35) // Adjust width dynamically
-                    .frame(height: geometry.size.height * 1.0) // Adjust height dynamically
+                    .frame(width: geometry.size.width * 0.35)
+                    .frame(height: geometry.size.height * 1.0)
                     .padding(.trailing, 5)
             }
         }
@@ -118,9 +112,9 @@ struct TimetableMainView: View {
     
     private var tabContentView: some View {
         VStack {
-            if selectedTab == .timetable {
+            if selectedTab == "시간표" {
                 timetableView
-            } else if selectedTab == .credits {
+            } else if selectedTab == "이수학점" {
                 creditsView
             }
         }
@@ -134,7 +128,7 @@ struct TimetableMainView: View {
                     .padding(.leading, 30)
                 Spacer()
                 Button(action: {
-                    isMenuOpen.toggle()
+                    stackPath.append(Route.timetableMenu)
                 }) {
                     Image(systemName: "ellipsis")
                         .foregroundStyle(.black)
@@ -162,30 +156,88 @@ struct TimetableMainView: View {
                 .fill(Color(red: 0.96, green: 0.96, blue: 0.98))
                 .frame(height: 50)
                 .overlay(
-                    HStack(spacing: 0) {
-                        ForEach(["총 이수학점", "전공", "교양필수", "교양"], id: \.self) { text in
-                            CustomPickerView(text: text, selectedOption: $selectedPickerIndex)
-                        }
-                        .padding(.horizontal, 5)
+                    HStack(spacing: 5) {
+                        customPickerView(text: "총 이수학점", selectedOption: $currSelectedOption, namespace: namespace)
+                        customPickerView(text: "전공", selectedOption: $currSelectedOption, namespace: namespace)
+                        customPickerView(text: "교양필수", selectedOption: $currSelectedOption, namespace: namespace)
+                        customPickerView(text: "교양", selectedOption: $currSelectedOption, namespace: namespace)
                     }
                         .padding(.horizontal, 5)
+                        .animation(.spring(), value: currSelectedOption)
                 )
                 .padding(.horizontal, 20)
             
-            HStack {
-                Text("졸업까지 필요한 학점")
-                    .font(.b14)
-                    .padding(.leading, 30)
-                Spacer()
+            if currSelectedOption == "총 이수학점" {
+                pieChart
+                HStack {
+                    Text("졸업까지 필요한 학점")
+                        .font(.b14)
+                        .padding(.leading, 30)
+                    Spacer()
+                }
+                
+                GraduationRequirementsRadarView(entries: ChartTransaction.allTransactions.map { RadarChartDataEntry(value: $0.quantity) })
+                    .frame(width: 300, height: 300)
             }
-            
-            GraduationRequirementsRadarView(entries: ChartTransaction.allTransactions.map { RadarChartDataEntry(value: $0.quantity) })
-                .frame(width: 300, height: 300)
+            if currSelectedOption == "전공" {
+                pieChart
+            }
         }
     }
     
     @ViewBuilder
-    private func tabButton(title: String, tab: TabType) -> some View {
+    private func customPickerView(text: String, selectedOption: Binding<String>, namespace: Namespace.ID) -> some View {
+        GeometryReader { geometry in
+            ZStack {
+                if selectedOption.wrappedValue == text {
+                    RoundedRectangle(cornerRadius: 30)
+                        .foregroundStyle(selectedOption.wrappedValue == text ? Color.white : Color.clear)
+                        .frame(height: 35)
+                        .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
+                        .matchedGeometryEffect(id: "selected", in: namespace)
+                }
+                
+                Text(text)
+                    .font(selectedOption.wrappedValue == text ? .sb14 : .m14)
+                    .foregroundStyle(selectedOption.wrappedValue == text ? Color("blue_4") : Color.black)
+                    .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
+            }
+            .contentShape(RoundedRectangle(cornerRadius: 30))
+            .onTapGesture {
+                selectedOption.wrappedValue = text
+            }
+        }
+    }
+    
+    private var pieChart: some View {
+        HStack {
+            PieChartView(slices: [
+                (majorCredit, Color(red: 0.39, green: 0.53, blue: 1)),
+                (GECredit, Color(red: 0.32, green: 0.69, blue: 0.03)),
+                (totalCredit - majorCredit - GECredit, .info02)
+            ], centerText: "SampleText\n \(Int(majorCredit + GECredit)) / \(Int(totalCredit))")
+            .frame(width: 150)
+            VStack {
+                HStack {
+                    Circle()
+                        .fill(Color(red: 0.39, green: 0.53, blue: 1))
+                        .frame(width: 12, height: 12)
+                    Text("전공 \(Int(majorCredit)) / \(majorCreditGoals)")
+                        .font(.m14)
+                }
+                HStack {
+                    Circle()
+                        .fill(Color(red: 0.32, green: 0.69, blue: 0.03))
+                        .frame(width: 12, height: 12)
+                    Text("교양 \(Int(GECredit)) / \(GECreditGoals)")
+                        .font(.m14)
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func tabButton(title: String, tab: String) -> some View {
         Button(action: {
             selectedTab = tab
         }) {
@@ -196,27 +248,47 @@ struct TimetableMainView: View {
     }
 }
 
-struct CustomPickerView: View {
-    var text: String
-    @Binding var selectedOption: String
+struct PieChartView: View {
+    @State var slices: [(Double, Color)]
+    var centerText: String = "sample text"
     
     var body: some View {
-        RoundedRectangle(cornerRadius: 30)
-            .fill(selectedOption == text ? Color.white : Color.clear)
-            .frame(height: 30)
-            .overlay(
-                Text(text)
-                    .font(.sb14)
-                    .foregroundStyle(selectedOption == text ? Color("blue_4") : Color.black)
-            )
-            .onTapGesture {
-                withAnimation {
-                    selectedOption = text
-                }
+        Canvas { context, size in
+            let donut = Path { p in
+                p.addEllipse(in: CGRect(origin: .zero, size: size))
+                p.addEllipse(in: CGRect(x: size.width * 0.1, y: size.height * 0.1, width: size.width * 0.8, height: size.height * 0.8))
             }
+            context.clip(to: donut, style: .init(eoFill: true))
+            
+            let total = slices.reduce(0) { $0 + $1.0 }
+            context.translateBy(x: size.width * 0.5, y: size.height * 0.5)
+            var pieContext = context
+            pieContext.rotate(by: .degrees(-90))
+            let radius = min(size.width, size.height) * 0.48
+            var startAngle = Angle.zero
+            for (value, color) in slices {
+                let angle = Angle(degrees: 360 * (value / total))
+                let endAngle = startAngle + angle
+                let path = Path { p in
+                    p.move(to: .zero)
+                    p.addArc(center: .zero, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+                    p.closeSubpath()
+                }
+                pieContext.fill(path, with: .color(color))
+                
+                startAngle = endAngle
+            }
+        }
+        .aspectRatio(1, contentMode: .fit)
+        .overlay(
+            Text(centerText)
+                .font(.m14)
+                .multilineTextAlignment(.center)
+        )
     }
 }
 
 #Preview {
     TimetableMainView(stackPath: .constant([.timetableMake]))
 }
+
