baa26a6a9ababa8a0ad8529b47b0acdda577764a
YeongJoon <yjk7557@naver.com>
2024-07-07 08:16:23 +0900

    [Refactor] FinalTimetableSelectorView 리팩토링

--- SATTO/Timetable/Views/TimetableAutoViews/Tabs/FinalTimetableSelectorView.swift
+++ SATTO/Timetable/Views/TimetableAutoViews/Tabs/FinalTimetableSelectorView.swift
@@ -16,90 +16,19 @@ struct FinalTimetableSelectorView: View {
     @State private var isProgressing = false
     @State private var errorPopup = false
     @State private var isRawChecked = false
+    @State private var currIndex: Int = 0
     
     @Binding var showingPopup: Bool
     @Binding var timetableIndex: Int
-    
-    @State private var currIndex: Int = 0
 
     var body: some View {
         ScrollView {
             LazyVStack {
-                if selectedValues.timetableList.count == 0 {
-                    if isRawChecked {
-                        Text("해당 전공 조합의 남는 시간에 맞는 교양 과목이 없어요..\n불가능한 시간대와 같은 제약조건을 완화해보세요.")
-                            .font(.sb16)
-                            .foregroundStyle(.blackWhite)
-                            .multilineTextAlignment(.center)
-                    } 
-                    else {
-                        Text("해당 전공 조합으로는 최적의 시간표가 존재하지 않아요..\n전체 시간표를 확인해보시겠어요?")
-                            .font(.sb16)
-                            .foregroundStyle(.blackWhite)
-                            .multilineTextAlignment(.center)
-                        Button(action: {
-                            isProgressing = true
-                            selectedValues.fetchFinalTimetableList(
-                                isRaw: true,
-                                GPA: selectedValues.credit,
-                                requiredLect: selectedValues.selectedSubjects,
-                                majorCount: selectedValues.majorNum,
-                                cyberCount: selectedValues.ELearnNum,
-                                impossibleTimeZone: selectedValues.selectedTimes,
-                                majorList: selectedValues.selectedMajorCombs) { result in
-                                    switch result {
-                                    case .success:
-                                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                                            self.isProgressing = false
-                                            self.isRawChecked = true
-                                        }
-                                    case .failure(let error):
-                                        print("Error: \(error)")
-                                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                                            self.isProgressing = false
-                                            self.errorPopup = true
-                                        }
-                                    }
-                                }
-                        }) {
-                            HStack {
-                                Text("전체 시간표 확인하기")
-                                Image(systemName: "arrow.clockwise")
-                            }
-                            .font(.sb16)
-                            .foregroundStyle(.blackWhite)
-                            .background(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .foregroundStyle(Color.menuCardBackground)
-                                    .shadow(color: colorScheme == .light ? Color(red: 0.65, green: 0.65, blue: 0.65).opacity(0.65) : Color.clear, radius: 6.23, x: 0, y: 1.22)
-                                    .padding(EdgeInsets(top: -10, leading: -10, bottom: -10, trailing: -10))
-                            )
-                            .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
-                        }
-                    }
+                if selectedValues.timetableList.isEmpty {
+                    emptyTimetableView
                 }
                 else {
-                    Text("\(selectedValues.timetableList.count)개의 시간표가 만들어졌어요!")
-                        .font(.sb14)
-                        .multilineTextAlignment(.center)
-                    Text("좌우로 스크롤하고 시간표를 클릭해 \n 원하는 시간표를 등록해요.")
-                        .font(.sb16)
-                        .multilineTextAlignment(.center)
-                    TabView(selection: $currIndex) {
-                        ForEach(selectedValues.timetableList.indices, id: \.self) { index in
-                            TimetableView(timetableBaseArray: selectedValues.timetableList[index])
-                                .onTapGesture {
-                                    timetableIndex = index
-                                    showingPopup.toggle()
-                                }
-                        }
-                    }
-                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
-                    .onAppear {
-                        currIndex = 0
-                        UIScrollView.appearance().isScrollEnabled = true //드래그제스처로 탭뷰 넘기는거 여기 TabView에서만 허용
-                    }
-                    .frame(height: 500)
+                    timetableTabView
                 }
             }
         }
@@ -129,9 +58,112 @@ struct FinalTimetableSelectorView: View {
                 .autohideIn(3)
         })
     }
+    
+    private var emptyTimetableView: some View {
+        VStack {
+            if isRawChecked {
+                Text("해당 전공 조합의 남는 시간에 맞는 교양 과목이 없어요..\n불가능한 시간대와 같은 제약조건을 완화해보세요.")
+                    .font(.sb16)
+                    .foregroundStyle(.blackWhite)
+                    .multilineTextAlignment(.center)
+            } else {
+                Text("해당 전공 조합으로는 최적의 시간표가 존재하지 않아요..\n전체 시간표를 확인해보시겠어요?")
+                    .font(.sb16)
+                    .foregroundStyle(.blackWhite)
+                    .multilineTextAlignment(.center)
+                
+                Button(action: fetchAllTimetables) {
+                    HStack {
+                        Text("전체 시간표 확인하기")
+                        Image(systemName: "arrow.clockwise")
+                    }
+                    .font(.sb16)
+                    .foregroundStyle(.blackWhite)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundStyle(Color.menuCardBackground)
+                            .shadow(color: colorScheme == .light ? Color(red: 0.65, green: 0.65, blue: 0.65).opacity(0.65) : Color.clear, radius: 6.23, x: 0, y: 1.22)
+                            .padding(EdgeInsets(top: -10, leading: -10, bottom: -10, trailing: -10))
+                    )
+                    .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+                }
+            }
+        }
+    }
+    
+    private var timetableTabView: some View {
+        LazyVStack {
+            Text("\(selectedValues.timetableList.count)개의 시간표가 만들어졌어요!")
+                .font(.sb14)
+                .multilineTextAlignment(.center)
+            Text("좌우로 스크롤하고 시간표를 클릭해 \n 원하는 시간표를 등록해요.")
+                .font(.sb16)
+                .multilineTextAlignment(.center)
+            TabView(selection: $currIndex) {
+                ForEach(selectedValues.timetableList.indices, id: \.self) { index in
+                    TimetableView(timetableBaseArray: selectedValues.timetableList[index])
+                        .onTapGesture {
+                            timetableIndex = index
+                            showingPopup.toggle()
+                        }
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+            .onAppear {
+                currIndex = 0
+                UIScrollView.appearance().isScrollEnabled = true //드래그제스처로 탭뷰 넘기는거 여기 TabView에서만 허용
+            }
+            .frame(height: 500)
+        }
+    }
+    
+    private func fetchAllTimetables() {
+        isProgressing = true
+        selectedValues.fetchFinalTimetableList(
+            isRaw: true,
+            GPA: selectedValues.credit,
+            requiredLect: selectedValues.selectedSubjects,
+            majorCount: selectedValues.majorNum,
+            cyberCount: selectedValues.ELearnNum,
+            impossibleTimeZone: selectedValues.selectedTimes,
+            majorList: selectedValues.selectedMajorCombs) { result in
+                switch result {
+                case .success:
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        isProgressing = false
+                        isRawChecked = true
+                    }
+                case .failure(let error):
+                    print("Error: \(error)")
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        isProgressing = false
+                        errorPopup = true
+                    }
+                }
+            }
+    }
 }
 
+
 #Preview {
-    FinalTimetableSelectorView(selectedValues: SelectedValues(), showingPopup: .constant(false), timetableIndex: .constant(1))
-        .preferredColorScheme(.light)
+//    FinalTimetableSelectorView(selectedValues: SelectedValues(), showingPopup: .constant(false), timetableIndex: .constant(1))
+//        .preferredColorScheme(.light)
+    LazyVStack {
+        Text("1개의 시간표가 만들어졌어요!")
+            .font(.sb14)
+            .multilineTextAlignment(.center)
+        Text("좌우로 스크롤하고 시간표를 클릭해 \n 원하는 시간표를 등록해요.")
+            .font(.sb16)
+            .multilineTextAlignment(.center)
+        TabView {
+            ForEach(0...3, id: \.self) { index in
+                TimetableView(timetableBaseArray: [])
+            }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+        .onAppear {
+            UIScrollView.appearance().isScrollEnabled = true //드래그제스처로 탭뷰 넘기는거 여기 TabView에서만 허용
+        }
+        .frame(height: 500)
+    }
 }
