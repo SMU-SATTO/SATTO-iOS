ad82c23439c872bf63554b68a2c7811b69297418
insung0927 <78968690+Fiddich-Dev@users.noreply.github.com>
2024-09-01 19:03:04 +0900

    [#84] 홈 UI재배치

--- SATTO/Home/HomeView.swift
+++ SATTO/Home/HomeView.swift
@@ -32,31 +32,21 @@ struct HomeView: View {
     @State var showSafari = false
     @State var urlString = "https://www.smu.ac.kr/kor/life/notice.do"
     
+    @State var showSWNoti = false
+    @State var showSWNotiURL = "https://swai.smu.ac.kr/bbs/board.php?bo_table=07_01"
+    
     @EnvironmentObject var navPathFinder: HomeNavigationPathFinder
     
     @EnvironmentObject var authViewModel: AuthViewModel
     
     @StateObject var friendViewModel = FriendViewModel()
     
+    @Binding var selectedTab: Tab
+    
     var body: some View {
         NavigationStack(path: $navPathFinder.path) {
             ScrollView {
                 VStack(alignment: .leading, spacing: 0) {
-                    
-//                    HStack(spacing: 0) {
-//                        Image("menu")
-//                        
-//                        Spacer()
-//                        
-//                        Image("alarm")
-//                            .padding(.trailing, 20)
-//                        
-//                        Image("account")
-//                    }
-//                    .padding(.horizontal, 20)
-//                    .padding(.bottom, 13)
-                    
-                    
                     
                     HStack(spacing: 0) {
                         VStack(spacing: 0) {
@@ -87,15 +77,9 @@ struct HomeView: View {
                             .fill(Color(red: 0.73, green: 0.83, blue: 0.98))
                         
                     )
+                    .padding(.bottom, 16)
                     
-//                    TimeTableTutorial(friendViewModel: friendViewModel)
                     
-                    CircularProgressView()
-                        .frame(width: 100)
-                        .padding(.bottom, 38)
-                        .padding(.leading, 20)
-                    
-                    // b14
                     Text("중요한 정보 모아 보기")
                         .font(.b14)
                         .padding(.leading, 20)
@@ -103,10 +87,14 @@ struct HomeView: View {
                     
                     ScrollView(.horizontal, showsIndicators: false) {
                         
+                    
                         HStack(spacing: 0) {
+                            
+                            Spacer()
+                                .frame(width: 20)
+                            
                             Button(action: {
                                 showSafari = true
-//                                navPathFinder.addPath(route: .academicNotice)
                             }, label: {
                                 HStack(alignment: .top, spacing: 0) {
                                     Text("학사 공지")
@@ -124,40 +112,52 @@ struct HomeView: View {
                                 )
                                 
                             })
-                            .padding(.leading, 20)
                             
+                            Spacer()
+                                .frame(width: 20)
                             
-                            HStack(alignment: .top, spacing: 0) {
-                                Text("이벤트")
-                                    .font(.sb14)
+                            Button(action: {
+                                showSWNoti = true
+                            }, label: {
+                                HStack(alignment: .top, spacing: 0) {
+                                    Text("sw 공지")
+                                        .font(.sb14)
+                                    
+                                    Image("st_noti 1")
+                                }
+                                .padding(.vertical, 12)
+                                .padding(.leading, 12)
+                                .background(
+                                    Rectangle()
+                                        .fill(Color(red: 0.99, green: 0.93, blue: 0.99))
+                                        .frame(width: 150, height: 100)
+                                        .cornerRadius(10)
+                                )
                                 
-                                Image("st_event1 1")
-                            }
-                            .padding(.vertical, 12)
-                            .padding(.leading, 12)
-                            .background(
-                                Rectangle()
-                                    .fill(Color(red: 0.98, green: 0.94, blue: 0.84))
-                                    .frame(width: 150, height: 100)
-                                    .cornerRadius(10)
-                            )
-                            .padding(.leading, 20)
+                            })
                             
-                            HStack(alignment: .top, spacing: 0) {
-                                Text("버스 정보")
-                                    .font(.sb14)
-                                
-                                Image("st_noti 1")
-                            }
-                            .padding(.vertical, 12)
-                            .padding(.leading, 12)
-                            .background(
-                                Rectangle()
-                                    .fill(Color(red: 0.99, green: 0.93, blue: 0.99))
-                                    .frame(width: 150, height: 100)
-                                    .cornerRadius(10)
-                            )
-                            .padding(.leading, 20)
+                            Spacer()
+                                .frame(width: 20)
+                            
+                            Button(action: {
+                                selectedTab = .event
+                            }, label: {
+                                HStack(alignment: .top, spacing: 0) {
+                                    Text("이벤트")
+                                        .font(.sb14)
+                                    
+                                    Image("st_event1 1")
+                                }
+                                .padding(.vertical, 12)
+                                .padding(.leading, 12)
+                                .background(
+                                    Rectangle()
+                                        .fill(Color(red: 0.98, green: 0.94, blue: 0.84))
+                                        .frame(width: 150, height: 100)
+                                        .cornerRadius(10)
+                                )
+                            })
+                            
                             
                             Spacer()
                                 .frame(width: 20)
@@ -166,44 +166,84 @@ struct HomeView: View {
                         
                         
                     }
-                    .padding(.bottom, 38)
+                    .padding(.bottom, 30)
+                    
+                    if friendViewModel.timeTables.isEmpty {
+                        Text("시간표가 없습니다")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.horizontal, 20)
+                            .foregroundStyle(Color.cellText)
+                    }
+                    else {
+                        HStack(spacing: 0) {
+                            Picker("Select Semester Year", selection: $friendViewModel.selectedSemesterYear) {
+                                ForEach(friendViewModel.getSemestersFromTimetables(timeTables: friendViewModel.timeTables), id: \.self) { semester in
+                                    Text(friendViewModel.formatSemesterString(semester: semester))
+                                        .tag(semester)
+                                }
+                            }
+                            
+                            Picker("Select Time Table", selection: $friendViewModel.selectedTimeTableName) {
+                                ForEach(friendViewModel.getTimetableNamesForSemester(timeTables: friendViewModel.timeTables, semester: friendViewModel.selectedSemesterYear), id: \.self) { name in
+                                    Text(name).tag(name)
+                                }
+                            }
+                            Spacer()
+                        }
+                        .padding(.horizontal, 20)
+                        .tint(Color.cellText)
+                    }
+                    TimeTableTutorial(friendViewModel: friendViewModel)
                     
                     // b14
-                    Text("친구들 시간표 보러 가기")
-                        .font(.b14)
-                        .padding(.bottom, 16)
-                        .padding(.leading, 20)
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(spacing: 0) {
-                            
-                            ForEach(friendViewModel.following, id: \.studentId) { friend in
-                                VStack(spacing: 0) {
-                                    Circle()
-                                        .frame(width: 60)
-                                        .padding(.bottom, 7)
-                                    
-                                    Text(friend.name)
-                                        .font(.m10)
-                                }
-                                .padding(.leading, 20)
-                            }
-                            Spacer()
-                                .frame(width: 20)
-                        }
-                    }
+//                    Text("친구들 시간표 보러 가기")
+//                        .font(.b14)
+//                        .padding(.bottom, 16)
+//                        .padding(.leading, 20)
+//
+//                    ScrollView(.horizontal, showsIndicators: false) {
+//                        HStack(spacing: 0) {
+//
+//                            ForEach(friendViewModel.following, id: \.studentId) { friend in
+//                                VStack(spacing: 0) {
+//                                    Circle()
+//                                        .frame(width: 60)
+//                                        .padding(.bottom, 7)
+//
+//                                    Text(friend.name)
+//                                        .font(.m10)
+//                                }
+//                                .padding(.leading, 20)
+//                            }
+//                            Spacer()
+//                                .frame(width: 20)
+//                        }
+//                    }
                     
                 }
             }
             .onAppear {
+                print("홈뷰 생성")
                 authViewModel.userInfoInquiry {
                     friendViewModel.fetchMyFollowingList(studentId: authViewModel.user.studentId ?? "asd") {
                         friendViewModel.following = friendViewModel.myFollowing
                     }
                 }
+                friendViewModel.fetchMyTimetableList()
+            }
+            // 학기를 바꾸면 그 학기에 해당하는 시간표들의 첫번째 값을 보여준다
+            .onChange(of: friendViewModel.selectedSemesterYear) { _ in
+                friendViewModel.selectedTimeTableName = friendViewModel.getTimetableNamesForSemester(timeTables: friendViewModel.timeTables, semester: friendViewModel.selectedSemesterYear).first ?? "이름없음"
+            }
+            // 시간표 이름을 바꾸면 그 시간표의 값을 보여준다
+            .onChange(of: friendViewModel.selectedTimeTableName) { _ in
+                friendViewModel.fetchTimeTableInfo(timeTableId: friendViewModel.getSelectedTimetableId(timeTables: friendViewModel.timeTables))
             }
             .fullScreenCover(isPresented: $showSafari) {
                 SafariView(url:URL(string: self.urlString)!)
+            }
+            .fullScreenCover(isPresented: $showSWNoti) {
+                SafariView(url:URL(string: self.showSWNotiURL)!)
             }
             .navigationDestination(for: HomedRoute.self) { route in
                 switch route {
@@ -284,7 +324,7 @@ struct CircularProgressView: View {
     }
 }
 
-#Preview {
-    HomeView()
-        .environmentObject(HomeNavigationPathFinder.shared)
-}
+//#Preview {
+//    HomeView()
+//        .environmentObject(HomeNavigationPathFinder.shared)
+//}
