8f58519c7958f1711564bf2dc7bdd16c73033a8f
YeongJoon <yjk7557@naver.com>
2024-10-04 15:53:03 +0900

    [Refactor] TimetableMainView 리팩토링
    안쓰는 변수 제거
    긴 코드 함수화 및 정리
    UI 수정

--- SATTO/Timetable/Views/TimetableMainView.swift
+++ SATTO/Timetable/Views/TimetableMainView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import PopupView
 
 enum TimetableRoute: Hashable {
     case timetableAuto
@@ -17,129 +16,41 @@ enum TimetableRoute: Hashable {
 }
 
 struct TimetableMainView: View {
-    @State var tabbarIsHidden: Bool = false
-    
     @State var stackPath = [TimetableRoute]()
+    
+    @State var tabbarIsHidden: Bool = false
     
     @ObservedObject var timetableMainViewModel: TimetableMainViewModel
     @ObservedObject var lectureSearchViewModel: LectureSearchViewModel
     @ObservedObject var constraintsViewModel: ConstraintsViewModel
     
-    @State private var selectedTab = "시간표"
-    @State private var currSelectedOption = "총 이수학점"
-    @State private var tempTimetableName = ""
-    
-    @Namespace private var namespace
+    @State private var tempTimetableName = ""  ///수정할 timetableName
     
     @State private var bottomSheetPresented = false
-    
-    let majorCreditGoals = 72                  /// 전공 학점 목표
-    @State var majorCredit: Double = 60        /// 전공 학점
-    let GECreditGoals = 60                     /// 교양 학점 목표
-    @State var GECredit: Double = 33           /// 교양 학점
-    @State var totalCredit: Double = 130       /// 전체 학점
-    
     @State private var nameModifyAlert = false
-    @State private var timetableIsPrivateAlert = false
-    @State private var deleteTableAlert = false
+    @State private var isPublicAlert = false
+    @State private var deleteAlert = false
     @State private var representAlert = false
     
     @Environment(\.colorScheme) private var colorScheme
     
     var body: some View {
-        NavigationStack(path: $stackPath){
+        NavigationStack(path: $stackPath) {
             ZStack {
                 Color.backgroundDefault
                     .ignoresSafeArea(.all)
                 VStack {
-                    headerView
+                    smuBanner
                     timetableView
                     Spacer()
                 }
             }
-            .sheet(isPresented: $bottomSheetPresented, content: {
-                ZStack {
-                    UnevenRoundedRectangle(cornerRadii: .init(topLeading: 20, topTrailing: 20))
-                        .foregroundStyle(Color.popupBackground)
-                        .ignoresSafeArea(.all)
-                    VStack(spacing: 15) {
-                        HStack {
-                            Button(action: {
-                                nameModifyAlert = true
-                                tempTimetableName = timetableMainViewModel.currentTimetable?.name ?? ""
-                            }) {
-                                HStack {
-                                    Image(systemName: "pencil")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 20, height: 16)
-                                    Text("이름 변경")
-                                        .font(.sb16)
-                                }
-                            }
-                            .foregroundStyle(.blackWhite200)
-                            Spacer()
-                        }
-                        HStack {
-                            Button(action: {
-                                representAlert = true
-                            }) {
-                                HStack {
-                                    Image(systemName: "crown")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 20, height: 16)
-                                    Text("대표시간표 변경")
-                                        .font(.sb16)
-                                }
-                            }
-                            .foregroundStyle(.blackWhite200)
-                            Spacer()
-                        }
-                        HStack {
-                            Button(action: {
-                                timetableIsPrivateAlert = true
-                            }) {
-                                HStack {
-                                    Image(systemName: "lock")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 20, height: 16)
-                                    Text("공개 범위 변경")
-                                        .font(.sb16)
-                                }
-                            }
-                            .foregroundStyle(.blackWhite200)
-                            Spacer()
-                        }
-                        HStack {
-                            Button(action: {
-                                deleteTableAlert = true
-                            }) {
-                                HStack {
-                                    Image(systemName: "trash")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 20, height: 16)
-                                    Text("삭제")
-                                        .font(.sb16)
-                                }
-                            }
-                            .foregroundStyle(.blackWhite200)
-                            Spacer()
-                        }
-                    }
-                    .padding(.leading, 30)
-                }
-                .presentationDetents([.height(150)])
-            })
+            .sheet(isPresented: $bottomSheetPresented, content: { timetableSettingSheet })
             .alert("수정할 이름을 입력해주세요", isPresented: $nameModifyAlert) {
                 TextField(timetableMainViewModel.currentTimetable?.name ?? "", text: $tempTimetableName)
                     .autocorrectionDisabled()
                 HStack {
-                    Button("취소", role: .cancel) {
-
-                    }
+                    Button("취소", role: .cancel, action: {})
                     Button("확인") {
                         Task {
                             guard let timetableId = timetableMainViewModel.currentTimetable?.id else { return }
@@ -162,7 +73,7 @@ struct TimetableMainView: View {
                     Button("취소", role: .cancel, action: {})
                 }
             }
-            .alert("시간표 공개 범위 변경", isPresented: $timetableIsPrivateAlert) {
+            .alert("시간표 공개 범위 변경", isPresented: $isPublicAlert) {
                 HStack {
                     Button("공개") {
                         Task {
@@ -179,7 +90,7 @@ struct TimetableMainView: View {
                     Button("취소", role: .cancel, action: {})
                 }
             }
-            .alert("정말로 삭제하시겠어요?", isPresented: $deleteTableAlert) {
+            .alert("정말로 삭제하시겠어요?", isPresented: $deleteAlert) {
                 HStack {
                     Button("아니요", role: .cancel, action: {})
                     Button("네") {
@@ -220,7 +131,7 @@ struct TimetableMainView: View {
         }
     }
     
-    private var headerView: some View {
+    private var smuBanner: some View {
         GeometryReader { geometry in
             let width = geometry.size.width
             let height = min(width * 1920 / 1080, 180)
@@ -235,8 +146,9 @@ struct TimetableMainView: View {
                 )
                 .foregroundStyle(Color.banner)
                 .overlay(
-                    ZStack {
-                        headerContent
+                    HStack {
+                        bannerContent
+                        Spacer()
                     }
                 )
                 .clipped()
@@ -244,81 +156,37 @@ struct TimetableMainView: View {
         .frame(height: 180)
     }
     
-    private var headerContent: some View {
+    private var bannerContent: some View {
         VStack(alignment: .leading, spacing: 15) {
-            headerMessage
-            createTimetableButton
+            VStack(alignment: .leading, spacing: 3) {
+                Text("2024년 2학기 시간표가 업로드됐어요!")
+                    .font(.b16)
+                    .shadow(color: colorScheme == .light ? .white : .black, radius: 1, x: 1, y: 1)
+                    .shadow(color: colorScheme == .light ? .white : .black, radius: 1, x: -1, y: -1)
+                Text("지금 시간표를 만들어보세요!")
+                    .font(.m12)
+                    .foregroundStyle(Color.bannerText)
+            }
+            Button(action: {
+                stackPath.append(TimetableRoute.timetableOption)
+            }) {
+                Text("시간표 만들기 ->")
+                    .font(.sb12)
+                    .foregroundStyle(.white)
+                    .background(
+                        RoundedRectangle(cornerRadius: 7)
+                            .fill(Color.buttonBlue)
+                            .padding(EdgeInsets(top: -10, leading: -15, bottom: -10, trailing: -15))
+                    )
+            }
+            .padding(EdgeInsets(top: 10, leading: 15, bottom: 10, trailing: 15))
         }
         .padding(.leading, 20)
     }
     
-    private var headerMessage: some View {
-        VStack(alignment: .leading, spacing: 3) {
-            Text("2024년 2학기 시간표가 업로드됐어요!")
-                .font(.b16)
-                .shadow(color: colorScheme == .light ? .white : .black, radius: 1, x: 1, y: 1)
-                .shadow(color: colorScheme == .light ? .white : .black, radius: 1, x: -1, y: -1)
-            Text("지금 시간표를 만들어보세요!")
-                .font(.m12)
-                .foregroundStyle(Color.bannerText)
-        }
-    }
-    
-    private var createTimetableButton: some View {
-        Button(action: {
-            stackPath.append(TimetableRoute.timetableOption)
-        }) {
-            Text("시간표 만들기 ->")
-                .font(.sb12)
-                .foregroundStyle(.white)
-                .background(
-                    RoundedRectangle(cornerRadius: 7)
-                        .fill(Color.buttonBlue)
-                        .padding(EdgeInsets(top: -10, leading: -15, bottom: -10, trailing: -15))
-                )
-        }
-        .padding(EdgeInsets(top: 10, leading: 15, bottom: 10, trailing: 15))
-    }
-    
     private var timetableView: some View {
         VStack {
-            HStack {
-                if let currentTimetable = timetableMainViewModel.currentTimetable {
-                    Text("\(currentTimetable.semester) \(currentTimetable.name)")
-                        .font(.b14)
-                        .padding(.leading, 30)
-                }
-                Spacer()
-                Group {
-                    if let _ = timetableMainViewModel.currentTimetable {
-                        Button(action: {
-                            stackPath.append(TimetableRoute.timetableModify)
-                        }) {
-                            Image(systemName: "pencil")
-                                .foregroundStyle(Color.blackWhite)
-                        }
-                        Button(action: {
-                            bottomSheetPresented.toggle()
-                        }) {
-                            Image(systemName: "gearshape")
-                                .foregroundStyle(Color.blackWhite)
-                        }
-                    }
-                }
-                    
-                Button(action: {
-                    stackPath.append(TimetableRoute.timetableList)
-                }) {
-                    Image(systemName: "ellipsis")
-                        .foregroundStyle(Color.blackWhite)
-                }
-                .padding(.trailing, 20)
-            }
-            .task {
-                if timetableMainViewModel.currentTimetable?.id == nil  {
-                    await timetableMainViewModel.fetchRepresentTimetable()
-                }
-            }
+            timetableHeader
             .padding(.top, 10)
             if let currentTimetable = timetableMainViewModel.currentTimetable {
                 TimetableView(timetable: currentTimetable)
@@ -331,10 +199,124 @@ struct TimetableMainView: View {
                     .padding()
             }
         }
+        .task {
+            if timetableMainViewModel.currentTimetable?.id == nil  {
+                await timetableMainViewModel.fetchRepresentTimetable()
+            }
+        }
+    }
+    
+    private var timetableHeader: some View {
+        HStack {
+            if let currentTimetable = timetableMainViewModel.currentTimetable {
+                Text("\(currentTimetable.semester) \(currentTimetable.name)")
+                    .font(.b14)
+                    .padding(.leading, 30)
+                Spacer()
+                Button(action: {
+                    stackPath.append(TimetableRoute.timetableModify)
+                }) {
+                    Image(systemName: "pencil")
+                        .foregroundStyle(Color.blackWhite)
+                }
+                Button(action: {
+                    bottomSheetPresented.toggle()
+                }) {
+                    Image(systemName: "gearshape")
+                        .foregroundStyle(Color.blackWhite)
+                }
+            } else {
+                Spacer()
+            }
+                
+            Button(action: {
+                stackPath.append(TimetableRoute.timetableList)
+            }) {
+                Image(systemName: "ellipsis")
+                    .foregroundStyle(Color.blackWhite)
+            }
+            .padding(.trailing, 20)
+        }
+    }
+    
+    private var timetableSettingSheet: some View {
+        ZStack {
+            UnevenRoundedRectangle(cornerRadii: .init(topLeading: 20, topTrailing: 20))
+                .foregroundStyle(Color.popupBackground)
+                .ignoresSafeArea(.all)
+            VStack(spacing: 15) {
+                HStack {
+                    Button(action: {
+                        nameModifyAlert = true
+                        tempTimetableName = timetableMainViewModel.currentTimetable?.name ?? ""
+                    }) {
+                        HStack {
+                            Image(systemName: "pencil")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 20, height: 16)
+                            Text("이름 변경")
+                                .font(.sb16)
+                        }
+                    }
+                    .foregroundStyle(.blackWhite200)
+                    Spacer()
+                }
+                HStack {
+                    Button(action: {
+                        representAlert = true
+                    }) {
+                        HStack {
+                            Image(systemName: "crown")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 20, height: 16)
+                            Text("대표시간표 변경")
+                                .font(.sb16)
+                        }
+                    }
+                    .foregroundStyle(.blackWhite200)
+                    Spacer()
+                }
+                HStack {
+                    Button(action: {
+                        isPublicAlert = true
+                    }) {
+                        HStack {
+                            Image(systemName: "lock")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 20, height: 16)
+                            Text("공개 범위 변경")
+                                .font(.sb16)
+                        }
+                    }
+                    .foregroundStyle(.blackWhite200)
+                    Spacer()
+                }
+                HStack {
+                    Button(action: {
+                        deleteAlert = true
+                    }) {
+                        HStack {
+                            Image(systemName: "trash")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 20, height: 16)
+                            Text("삭제")
+                                .font(.sb16)
+                        }
+                    }
+                    .foregroundStyle(.blackWhite200)
+                    Spacer()
+                }
+            }
+            .padding(.leading, 30)
+        }
+        .presentationDetents([.height(150)])
     }
 }
-//
-//#Preview {
-//    TimetableMainView()
-//        .preferredColorScheme(.light)
-//}
+
+#Preview {
+    TimetableMainView(timetableMainViewModel: TimetableMainViewModel(container: .preview), lectureSearchViewModel: LectureSearchViewModel(container: .preview), constraintsViewModel: ConstraintsViewModel(container: .preview))
+}
